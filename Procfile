97f5241270959154acaa123e62ff98957f08cf3e
raghavibarla02 <manu.naidu2005@gmail.com>
2026-09-06 11:30:03 +0530

    Add Procfile and fix settings for Render deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn orca_project.wsgi:application --bind 0.0.0.0:$PORT
--- orca_project/settings.py
+++ orca_project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-07#m#&z*23jro!_ct9g$3p9)r0=fom2=s%g1bc(428uadr^!i&'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-07#m#&z*23jro!_ct9g$3p9)r0=fom2=s%g1bc(428uadr^!i&')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +122,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
